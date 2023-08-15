73765d3c875279e65f447802dd8f2e6cc5b2f9cc
theusmon0v <asadbek.usmonoff2003@gmail.com>
2023-08-15 19:34:53 +0500

    Register vaqti bodydan keladigan malumot password hashlash uchun funksiya yozdim va user malumotlari to'g'ri bo'lsa token qaytadigan qildim

---
+++ src/utils/helper.js
@@ -0,0 +1,32 @@
+import bcrypt, { compare, hash, hashSync } from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config"
+
+let salt = 10;
+export const bcryptHash = {
+      hash: (passwd) => {
+        return bcrypt.hashSync(passwd, salt)
+      },
+      compare: (passwd, hash) => {
+        return bcrypt.compareSync(hash, passwd)
+      }
+}
+
+
+
+export const jwtSignToken = {
+    sign: (payload) => {
+        return jwt.sign(payload, process.env.SECRET_KEY);
+    }
+}
+
+export const jwtVerifyToken = {
+    verify: (token, SECRET_KEY) => {
+        try {
+            return jwt.verify(token, SECRET_KEY);
+        } catch (err) {
+            console.error('JWT malumot yechib olishdagi xatolik', err.message);
+            return null; 
+        }
+    }
+}
