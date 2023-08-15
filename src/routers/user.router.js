b03367b0f4d17aa9048f4a4c6f33c5d67021394b
theusmon0v <asadbek.usmonoff2003@gmail.com>
2023-08-15 15:01:24 +0500

    Router fayl ichida route ga bo'lib oldim

---
+++ src/routers/user.router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.post("/register")
+router.post("/login")
+router.get("/user")
+
+export default (router);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import "dotenv/config"
+import router from "./routers/user.router.js";
 
 
 const app = express();
+
+app.use(router)
  
+
+
+
 let port = process.env["PORT"] || 7000
 let host = process.env["HOST"] || "localhost"
-
 
 app.listen(port, () => console.log(`Server is running in http://${host}:${port}`))
